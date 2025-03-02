0cbfe24c43eb7b09eba7a6f316f3deb15c49676c
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-02 20:40:47 +0800

    added Ingredient, Recipe, RecipeIngredient models in models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+
+class RecipeIngredient(models.Model):
+    Quantity = models.BigIntegerField() # not sure
+    Ingredient = models.ForeignKey(Ingredient)
+    Recipe = models.ForeignKey(Recipe)
+
+
