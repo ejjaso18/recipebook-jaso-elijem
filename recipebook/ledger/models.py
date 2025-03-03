21733bf3f8088f1c95564a1c0bfae4cf9061a2aa
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-03 13:12:44 +0800

    added string representation and absolute urls for Ingredient and Recipe and added related_name for ingredient and recipe in RecipeIngredient

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,16 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('Ingredient', args=[str(self.pk)])
+
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('Recipe', args=[str(self.pk)])
+
 class RecipeIngredient(models.Model):
-    quantity = models.DecimalField(max_length=10) 
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=10, null=True) 
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE,
+        related_name='recipes')
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE,
+        related_name='ingredients')
+    
 
 
