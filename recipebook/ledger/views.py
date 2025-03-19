bf6795b698bfcc50fc6a3f9b9b7a6798a344976e
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-19 21:21:00 +0800

    modified views to make the page only visible to logged in users

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,15 +1,17 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Recipe
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipe_list.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe.html'
+
 
 
 
