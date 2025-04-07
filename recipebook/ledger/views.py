3d4b09705d15a11849c2e07b2db1b4407841a96c
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-04-07 23:56:01 +0800

    added a view for RecipeCreate

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,8 +1,10 @@
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 
+from . import models, forms
 from .models import Recipe
+from .forms import RecipeForm, IngredientForm, RecipeIngredientForm, RecipeImageForm
 
 class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
@@ -11,6 +13,26 @@ class RecipeListView(LoginRequiredMixin, ListView):
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe.html'
+    
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'recipe_create.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ingredient_form'] = IngredientForm()
+        context['recipe_ingredient_form'] = RecipeIngredientForm()
+        return context
+    
+    def form_valid(self, form):
+        profile = get_object_or_404(models.Profile, user=self.request.user)
+        form.instance.author = profile
+        return super().form_valid(form)
+
+        
+
+
 
 
 
