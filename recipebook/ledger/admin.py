e2f6d735280a8c2522f323f19b489dbb899869dc
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-04-07 20:40:36 +0800

    added RecipeImage to admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import Ingredient, Recipe, RecipeIngredient, Profile
+from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImage
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -29,8 +29,12 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
     list_filter = ('recipe', )
 
+class RecipeImageAdmin(admin.ModelAdmin):
+    model = RecipeImage
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(RecipeImage, RecipeImageAdmin)
