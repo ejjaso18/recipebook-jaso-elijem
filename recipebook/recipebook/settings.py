c0627baf754f1145dc88b4161d98c2cae8d242ce
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-04-10 00:09:49 +0800

    implemented bonus: hiding secret key

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -12,16 +12,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ap-!mahqsqhoe6v6&g&j0lg4m_jel@pg48#rdz$xk!_w2mar%o'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
